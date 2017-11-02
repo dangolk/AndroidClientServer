6a65f313af39440a4ec973ff8f7a70d0fb892ab7
Kiran Dangol <kiran_dan2009@yahoo.com>
2017-11-01 19:49:52 -0500

    Initial commmit

---
+++ app/src/main/java/com/example/dangolk/wolfchat/Constants.java
@@ -0,0 +1,9 @@
+package com.example.dangolk.wolfchat;
+
+/**
+ * Created by dangolk on 11/01/17.
+ */
+
+public class Constants {
+    public static final String IP_LOCAL_HOST = "http://10.26.114.144:3000";
+}
---
+++ app/src/main/java/com/example/dangolk/wolfchat/activities/BaseFragmentActivity.java
@@ -0,0 +1,33 @@
+package com.example.dangolk.wolfchat.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dangolk.wolfchat.R;
+
+/**
+ * Created by dangolk on 11/01/17.
+ */
+
+public abstract class BaseFragmentActivity extends AppCompatActivity{
+    abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_base);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.activity_fragment_base_fragmentContainer);
+
+        if(fragment == null){
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.activity_fragment_base_fragmentContainer, fragment)
+                        .commit();
+        }
+    }
+}
